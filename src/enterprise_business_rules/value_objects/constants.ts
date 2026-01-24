5e3c4119bb097ec33ede383e616ec0b3d890356f
Miguel Menguito <menguito.miggy@gmail.com>
2026-01-24 20:07:34 +0800

    [main] Update constants

--- src/enterprise_business_rules/value_objects/constants.ts
+++ src/enterprise_business_rules/value_objects/constants.ts
@@ -1,7 +1,15 @@
-import { ValentineMessage } from '../entities/ValentineMessage'
+export const BUTTON_LABELS = {
+  submit: 'Send Love',
+  cancel: 'Cancel',
+  reset: 'Reset Message',
+};
 
-export const presetMessages: ValentineMessage[] = [
-  new ValentineMessage('You are my sunshine!'),
-  new ValentineMessage('Happy Valentine’s Day, love!'),
-  new ValentineMessage('Forever yours, with all my heart.'),
-];
+export const GENERIC_MESSAGES = {
+  welcome: 'Welcome to the Valentine’s Card!',
+  defaultMessage: 'Happy Valentine’s Day! ❤️',
+  error: 'Oops! Something went wrong.',
+};
+
+export const HEART_ANIMATION = {
+  title: 'Beating Heart Animation',
+};
