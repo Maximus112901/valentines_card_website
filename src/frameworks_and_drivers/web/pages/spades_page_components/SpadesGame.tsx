acd73b533825edc5ccd47e4dd1c4a815f405b95c
Miguel Menguito <menguito.miggy@gmail.com>
2026-02-08 22:11:50 +0800

    [dev/alice-in-borderland-spades] Add logic for updating levels

--- src/frameworks_and_drivers/web/pages/spades_page_components/SpadesGame.tsx
+++ src/frameworks_and_drivers/web/pages/spades_page_components/SpadesGame.tsx
@@ -1,24 +1,67 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './SpadesGame.module.css'
 import { BUTTON_LABELS, SPADES_PAGE } from '../../constants';
+import { useAppState } from '../../context/AppStateContext';
+
+function generateUniqueRandomNumbers(count: number, min: number, max: number) {
+    const numbers = new Set();
+
+    while (numbers.size < count) {
+        const rand = Math.floor(Math.random() * (max - min + 1)) + min;
+        numbers.add(rand);
+    }
+
+    return Array.from(numbers);
+}
 
 export function SpadesGame() {
+    const { dispatch } = useAppState()
+
+    // Keep track of level and randomly generate numbers
     const gridSizes = [2, 3, 4]
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const [level, setLevel] = useState(1);
+    const [maxNumber, setMaxNumber] = useState(gridSizes[level - 1] ** 2);
+    const [numbers, setNumbers] = useState(generateUniqueRandomNumbers(maxNumber, 1, maxNumber))
+
+    useEffect(() => {
+        // if not yet last level
+        if (level <= gridSizes.length) {
+            const newMax = gridSizes[level - 1] ** 2;
+            setMaxNumber(newMax);
+            setNumbers(generateUniqueRandomNumbers(newMax, 1, newMax));
+
+            // reset board
+            setExpectedNumber(1);
+            setPressedNumbers([]);
+        }
+        // if player cleared previous level, they won
+        else {
+            dispatch({ suit: "spades", payload: true })
+        }
+    }, [level]);
 
     // Keep pressed and next numbers
     const [expectedNumber, setExpectedNumber] = useState(1);
     const [pressedNumbers, setPressedNumbers] = useState<number[]>([]);
 
     const handleTileClick = (num: number) => {
+        // if correct
         if (num === expectedNumber) {
             setExpectedNumber(expectedNumber + 1);
             setPressedNumbers([...pressedNumbers, num]);
+
+            // check last number was pressed correctly
+            if (num === maxNumber) {
+                setLevel(prev => prev + 1)
+            }
+        // if wrong
         } else {
+            setLevel(1);
             setPressedNumbers([]);
             setExpectedNumber(1);
         }
     };
+
 
     return (
         <div className={styles.gameContainer}>
@@ -27,13 +70,13 @@ export function SpadesGame() {
             <div
                 style={{
                     display: "grid",
-                    gridTemplateColumns: `repeat(${gridSizes[1]}, 1fr)`,
+                    gridTemplateColumns: `repeat(${gridSizes[level - 1]}, 1fr)`,
                 }}
             >
                 {numbers.map((num) => (
                     <div
                         key={num}
-                        onClick={() =>handleTileClick(num)}
+                        onClick={() => handleTileClick(num)}
                         className={pressedNumbers.includes(num) ? styles.pressedGridItem : styles.gridItem}
                     >
                         {num}
